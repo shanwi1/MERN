b6ce6f1648e815853ea6c4b589b7bfb36e867f67
Shanwi5 <shanwibm@gmail.com>
2026-08-22 13:16:10 +0530

    commit

--- react/src/pages/About.jsx
+++ react/src/pages/About.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Product from '../components/Product'
 
 function About() {
   return (
     <div>
       <h1>About Us</h1>
       <p>Welcome to our about page!</p>
+      <Product/>
     </div>
   )
 }
